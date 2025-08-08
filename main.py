5d27127435432f8c0af046e3f53c97652008f20a
Milos Stojko <mstojko@tenstorrent.com>
2025-08-08 12:06:17 +0000

    Sync to old old

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # pip install accelerate
 
+import time
 import sys
 import os
 sys.path.append(os.getcwd())
@@ -39,6 +40,8 @@ messages = [
     }
 ]
 
+t0 = time.perf_counter()
+
 inputs = processor.apply_chat_template(
     messages, add_generation_prompt=True, tokenize=True,
     return_dict=True, return_tensors="pt"
@@ -50,10 +53,13 @@ input_len = inputs["input_ids"].shape[-1]
 # print("Thread Name:", threading.current_thread().name)
 
 with torch.inference_mode():
-    generation = model.generate(**inputs, max_new_tokens=20, do_sample=False)
+    generation = model.generate(**inputs, max_new_tokens=1, do_sample=False)
     generation = generation[0][input_len:]
 
-print('average llm time: ', profiling_custom.llm_total_time / profiling_custom.llm_call_count, ' called', profiling_custom.llm_call_count, 'times')
+time_to_first_token = time.perf_counter() - t0
+print(f'{time_to_first_token=}')
+
+#print('average llm time: ', profiling_custom.llm_total_time / profiling_custom.llm_call_count, ' called', profiling_custom.llm_call_count, 'times')
 decoded = processor.decode(generation, skip_special_tokens=True)
 print(decoded)
 
